53beaac2382d2c808a7801511a32056fcb2b37f1
Scott Brumley <scott@nsisecure.com>
2018-07-08 16:37:19 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,91 @@
+FROM alpine
+
+
+RUN apk update
+
+#RUN apk add --no-cache python3 \
+#    && apk add curl \
+#    && apk add git \
+#    && apk add gcc \
+#    && apk add py-setuptools \
+#    && apk add py-pip3 \
+#    && rm -rf /var/cache/apk/*
+
+#    apk add python2-dev && \
+
+RUN apk add --no-cache python3 && \
+    apk add bash && \
+    apk add curl && \
+    apk add git && \
+    apk add gcc && \
+    apk add python3-dev && \
+    apk add musl-dev && \
+    python3 -m ensurepip && \
+    rm -r /usr/lib/python*/ensurepip && \
+    pip3 install --upgrade pip setuptools && \
+    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
+    if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
+    rm -r /root/.cache
+
+#RUN apk add --no-cache curl python pkgconfig python-dev openssl-dev libffi-dev musl-dev make gcc
+#RUN pip install setuptools
+
+# Set the lang, you can also specify it as as environment variable through docker-compose.yml
+ENV LANG=en_US.UTF-8 \
+    LANGUAGE=en_US.UTF-8
+
+
+# Install PyMISP 
+#RUN git clone https://github.com/MISP/PyMISP.git
+#RUN cd PyMISP/; python setup.py install
+
+# Install Requests
+RUN pip3 install requests
+
+### Python Common
+RUN echo "Installing Python Common"
+RUN pip3 install common
+RUN pip3 install Flask
+RUN pip3 install flask-socketio
+RUN pip3 install eventlet
+
+RUN python --version
+
+# Install OpenDXL Python client
+#RUN pip install cffi
+#RUN mkdir -p /config
+RUN git clone https://github.com/opendxl/opendxl-client-python.git
+RUN cd opendxl-client-python/; python setup.py install
+
+# Install OpenDXL bootstrap
+RUN git clone https://github.com/opendxl/opendxl-bootstrap-python.git
+RUN cd opendxl-bootstrap-python/;python setup.py install
+
+## Install OpenDXL TIE Client
+RUN echo "Installing Open DXL TIE Client"
+RUN git clone https://github.com/opendxl/opendxl-tie-client-python.git 
+RUN cd opendxl-tie-client-python && python setup.py install
+
+# Install OpenDXL MAR SDK
+#RUN git clone https://github.com/opendxl/opendxl-mar-client-python.git
+#RUN cd opendxl-mar-client-python/; python setup.py install
+
+ADD config/brokercerts.crt /config/brokercerts.crt
+ADD config/client.crt /config/client.crt
+ADD config/client.key /config/client.key
+ADD config/monitor.config /monitor.config 
+
+# Install MISP MAR script
+RUN git clone https://github.com/scottbrumley/opendxl_web_api.git
+
+
+## Clean UP
+#RUN apk del --no-cache git gcc linux-headers musl-dev
+
+## Setup Flask Environment
+ENV FLASK_APP=/opendxl_web_api/opendxl_web_api.py
+
+WORKDIR /opendxl_web_api
+ADD config/dxlclient.config dxlclient.config
+CMD ["scripts/service.sh","debug"]
+
---
+++ container.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+source vars
+
+case "$1" in
+
+'dxlclient')
+  echo "Install Python3"
+  sudo apt install -y python3
+  sudo apt install python3-pip
+  sudo pip3 install --upgrade pip setuptools
+  if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
+  if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
+  
+  echo "Adding DXL Client Config to Config directory"
+  rm -rf opendxl-client-python/
+  git clone https://github.com/opendxl/opendxl-client-python.git
+  cd opendxl-client-python/
+  python setup.py install
+  cp examples/dxlclient.config ../config
+  #cd ..
+  #python -m dxlclient updateconfig config ${epo} -t ${epoport} -u ${epouser} -p ${epopass} 
+;;
+
+'configclient')
+  echo "Building DXL Client Certs"
+  openssl genrsa -out config/ca.pem 2048
+  openssl req -new -x509 -days 365 -extensions v3_ca -keyout config/ca.key -out config/ca.crt -subj "/C=US/ST=Georgia/L=Atlanta/O=McAfee/OU=HQ/CN=misp-mar.mcafee.com" -passout pass:${pempass}
+  openssl genrsa -out config/client.key 2048
+  openssl req -out config/client.csr -key config/client.key -new -subj "/C=US/ST=Georgia/L=Atlanta/O=McAfee/OU=HQ/CN=misp-mar.mcafee.com" -passout pass:${pempass}
+  openssl x509 -req -in config/client.csr -CA config/ca.crt -CAkey config/ca.key -CAcreateserial -out config/client.crt -days 365
+;;
+
+# Stop and Remove
+'remove')
+echo "Stopping and Removing Container ${NAME}"
+sudo docker stop ${NAME};sudo docker rm ${NAME} 
+;;
+
+# Build Container
+'build')
+echo "Bulding Container ${NAME}"
+sudo docker build \
+    --rm=true --force-rm=true \
+    -t ${LOCATION} .
+;;
+
+# Run Container
+#    --network=host \
+'run')
+echo "Run Container ${NAME}"
+sudo docker run -it -d \
+    -e FLASK_TOKEN="${2}" \
+    --name ${NAME} \
+    --restart=unless-stopped \
+    -p 5000:5000 \
+   ${LOCATION} 
+;;
+
+# Debug Container
+'debug')
+echo "Debug Container ${NAME}"
+sudo docker run -it \
+    -e FLASK_TOKEN="${2}" \
+    --name ${NAME} \
+    --restart=unless-stopped \
+    -p 5000:5000 \
+   ${LOCATION} \
+   /bin/sh 
+;;
+
+# Purge All Containers
+'purge-all')
+echo "Purging All Containers ${NAME}"
+sudo docker system prune -a
+;;
+
+# Purge Stopped Containers
+'purge')
+echo "Purge Stopped Containers ${NAME}"
+sudo docker system prune
+;;
+
+*)
+echo "Things you can do with this script:"
+echo "Set container name and location in vars file"
+echo "./container dxlclient - Install DXL Client"
+echo "./container configclient - Build Client Certs"
+echo "./container init - Initialize Database"
+echo "./container build - Build Container"
+echo "./container remove - Remove Container"
+echo "./container run - Run Container"
+echo "./container purge-all - Purge all containers and images"
+echo "./container purge - Purge stopped containers and images"
+;;
+
+esac
