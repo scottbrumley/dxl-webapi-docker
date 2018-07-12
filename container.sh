69148138ae50603d58cc7f49ee72dfa41535a8fc
sbrumley <sbrumley@wireless-town.com>
2018-07-12 13:03:20 -0400

    Added ability to set client certificate common name in vars as well as pem_pass

--- container.sh
+++ container.sh
@@ -25,10 +25,10 @@ case "$1" in
 'configclient')
   echo "Building DXL Client Certs"
   openssl genrsa -out config/ca.pem 2048
-  openssl req -new -passout pass:${2} -x509 -days 365 -extensions v3_ca -keyout config/ca.key -out config/ca.crt -subj "/C=US/ST=Georgia/L=Atlanta/O=McAfee/OU=HQ/CN=misp-mar.mcafee.com"
+  openssl req -new -passout pass:${PEM_PASS} -x509 -days 365 -extensions v3_ca -keyout config/ca.key -out config/ca.crt -subj "/C=US/ST=Georgia/L=Atlanta/O=McAfee/OU=HQ/CN=${CLIENT_CERT}"
   openssl genrsa -out config/client.key 2048
-  openssl req -out config/client.csr -key config/client.key -new -subj "/C=US/ST=Georgia/L=Atlanta/O=McAfee/OU=HQ/CN=misp-mar.mcafee.com" -passout pass:${2}
-  openssl x509 -req -in config/client.csr -CA config/ca.crt -CAkey config/ca.key -CAcreateserial -out config/client.crt -days 365 -passin pass:${2}
+  openssl req -out config/client.csr -key config/client.key -new -subj "/C=US/ST=Georgia/L=Atlanta/O=McAfee/OU=HQ/CN=${CLIENT_CERT}" -passout pass:${PEM_PASS}
+  openssl x509 -req -in config/client.csr -CA config/ca.crt -CAkey config/ca.key -CAcreateserial -out config/client.crt -days 365 -passin pass:${PEM_PASS}
 ;;
 
 # Stop and Remove
